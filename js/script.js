c96669564a908d0d9d8bad29cd98937a108c3571
MdNAF <nayem01799597640@gmail.com>
2022-02-25 14:14:09 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(function(){
+  
+    // type js
+
+    var typed = new Typed('.type', {
+        strings: ['Designer',
+                  'Freelancer',
+                  'Front-End-Devloper'
+                ],
+        smartBackspace: true,
+        typeSpeed: 60,
+        backSpeed: 60,
+        loop: true,
+      });
+
+
+})
